1c2c9f53b8820e0b96402132879ab20ce152b673
Mario <mizar2502@aol.com>
2020-04-20 23:01:09 +0200

    Add player movement

---
+++ Player.cs
@@ -0,0 +1,54 @@
+using Godot;
+using System;
+
+public class Player : Area2D
+{
+    [Export]
+    public int speed = 400;
+
+    private Vector2 _screenSize;
+
+    public override void _Ready()
+    {
+        _screenSize = GetViewport().Size;
+    }
+
+    public override void _Process(float delta)
+    {
+        var velocity = new Vector2();
+
+        if(Input.IsActionPressed("ui_right"))
+        {
+            velocity.x += 1;
+        }
+        if(Input.IsActionPressed("ui_left"))
+        {
+            velocity.x -= 1;
+        }
+        if(Input.IsActionPressed("ui_down"))
+        {
+            velocity.y += 1;
+        }
+        if(Input.IsActionPressed("ui_up"))
+        {
+            velocity.y -= 1;
+        }
+
+        var animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        if(velocity.Length() > 0)
+        {
+            velocity = velocity.Normalized() * speed;
+            animatedSprite.Play();
+        }
+        else
+        {
+            animatedSprite.Stop();
+        }
+
+        Position += velocity * delta;
+        Position = new Vector2(
+            x: Mathf.Clamp(Position.x, 0, _screenSize.x),
+            y: Mathf.Clamp(Position.y, 0, _screenSize.y)
+        );
+    }
+}
